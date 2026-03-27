2d930c7fa43b89688a2c607cffa6803341db8a47
Humanities2026 <u.n2015@hotmail.com>
2026-03-27 18:03:01 +0400

    Add files via upload

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://jawmpnzvdiznomgfsxza.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imphd21wbnp2ZGl6bm9tZ2ZzeHphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MDU2MTgsImV4cCI6MjA5MDE4MTYxOH0.ZzpEsIzK3RiLtHeeOcWzDvSK2F_s2D8KZJP6W3cpRNU";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
